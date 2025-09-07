df69089bd669274a28fbdd3f0b3b9f1e6eb8bea0
wlkqyang-star <wlkqyang@gmail.com>
2025-09-07 21:17:22 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: '/format-converter/',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
+  base: '/format-converter/', // <-- 这里是关键！
 })
