89744d9a096abdbc53eea2db0a776ee6c6e53851
wlkqyang-star <wlkqyang@gmail.com>
2025-09-09 16:51:24 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -131,7 +131,7 @@ function App() {
       }
       
       // 调用后端API
-      const response = await fetch(`http://10.70.83.185:5000/api/convert/${selectedType} `, {
+      const response = await fetch(`http://121.40.64.241:5000/api/convert/${selectedType} `, {
         method: 'POST',
         body: formData
       } )
